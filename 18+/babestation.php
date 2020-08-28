7859245bbd2b1fd413a87b77ca247e0290f13d76
mbahnunung <mbahnunung@users.noreply.github.com>
2020-08-29 01:31:51 +0700

    Add files via upload

---
+++ 18+/babestation.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>BabeStation</title>
+    <script src="https://api.mbahnunungonline.org/library/CT+b1p+DEeSDCQ4AfQhyIQ.js"></script>
+        	
+</head>
+<body style="width:100%;height:100%;padding:0;margin:0;background-color:white;">
+<script language=JavaScript>
+<!--
+function clickIE() {if (document.all) {(message);return false;}}
+function clickNS(e) {if
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2||e.which==3) {(message);return false;}}}
+if (document.layers)
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+document.oncontextmenu=new Function("return false")
+// -->
+</script>
+<center>
+<div id="myElement">Loading the player...</div>
+<script type="text/javascript">
+    jwplayer("myElement").setup({
+        primary : 'flash',
+        width: '596',
+        height: '340',
+        fallback: false,
+        sharing: '{}',
+        skin : '',
+        abouttext: '*** mbah nunung Online ***',
+        aboutlink: 'http://mbahnunungonline.org',
+        autostart: 'true',
+        stretching: 'exactfit',
+        captions: {back: false, "default": false, fontsize: 14},
+        playlist: [{ 
+        title: 'BabeStation',       
+        image: 'http://www.babestation.tv/vegas/hotslots.jpg',       
+        sources: [
+            {file: 'http://46.105.116.159:1935/iptv_cellcast_24/livestream3/player.m3u8'},
+            {file: 'http://46.105.116.159:1935/iptv_cellcast_24/livestream3/player.m3u8'}
+            ],
+        captions: [
+            {file: 'lostcorners-en.vtt', label: 'Melik'},
+            {file: 'lostcorners-fr2.vtt', label: 'Dadapan'},
+            {file: 'lostcorners-nl.vtt', label: 'Gambor'}
+        ]
+            
+        }]
+    });
+</script>
+</center>                                                                                                                                                        
+</body>                                                                                                                                                          
+</html>
